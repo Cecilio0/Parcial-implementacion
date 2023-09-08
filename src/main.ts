841c8ff272613d4e5188bf09039f21431cda83ad
Dread9182 <84605991+Dread9182@users.noreply.github.com>
2023-09-07 19:00:03 -0500

    Creacion del proyecto

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const PORT: string = process.env.PORT;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+}
+bootstrap();
